16f7aaf381111d13c56aabfc4fef233f3223d59b
Nimish Gupta <nimish007gupta@gmail.com>
2025-03-08 17:27:17 +0530

    gitignore

--- frontend/src/services/service.tsx
+++ frontend/src/services/service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000/"; // localhost base url added
+const baseUrl = "http://127.0.0.1:8000/"; // backend localhost base url
 
 const FetchLocation = async () => {
 
